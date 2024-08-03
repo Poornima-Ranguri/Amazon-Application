efbf6ca8574d9372eaccb1236cf38213eb8ca6fe
Poornima Ranguri <122213541+Poornima-Ranguri@users.noreply.github.com>
2024-08-03 22:49:26 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,17 @@
+import React from "react"
+import './App.css';
+import Header from "./Header";
+
+function App() {
+  return (
+    <div className="App">
+        <h1>Hello AMAZON</h1>
+        <Header/>
+        {/* Home */}
+
+
+    </div>
+  );
+}
+
+export default App;
---
+++ firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAFG3oyVL2bu_8GXKhiBbrbAAasqt9OTB4",
+    authDomain: "clone-bccba.firebaseapp.com",
+    projectId: "clone-bccba",
+    storageBucket: "clone-bccba.appspot.com",
+    messagingSenderId: "26163311353",
+    appId: "1:26163311353:web:94aeed15d424198967e828",
+    measurementId: "G-Z205DET2LY"
+  };
